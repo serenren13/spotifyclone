da43d7f0b5cba9a8c79b1af003c80312987c113e
Lulu Wilson - lulukayw <lulu@kay-wilson.net>
2026-05-28 09:27:36 -0400

    cleaning up codebase

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 import { useSpotify } from "../context/SpotifyContext";
+// import '../styling/Profile.css';
 
 const api = axios.create({ baseURL: "http://127.0.0.1:5001/api" });
 
